018d8c43a6ebc5a22193aefa7b6353c20e0afeac
Ben Rosenberg <xethr@cli64>
2020-08-09 18:21:51 -0400

    done!

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # sent-latex
-attempt to make sent work with latex 
+attempt to make sent work with latex
+
+will:
+* allow you to have latex in your sent presentations
+* capitalize on sent's ability to display images by having it display a latex png
+* turn your sent txt file from one with latex into one with image references instead
+
+will not:
+* allow you to have latex in the same slide as regular text
+* allow you to use all the bells and whistles of latex within sent. if you want that, use [beamer](https://ctan.org/pkg/beamer?lang=en) smh
+
+## dependencies:
+* [sent](https://tools.suckless.org/sent/) (obviously)
+* [farbfeld](https://tools.suckless.org/farbfeld/)
+* [python](https://www.python.org/)
+* [latex](https://www.latex-project.org/get/) and pdflatex
+* [imagemagick](https://imagemagick.org/index.php)
+
+## testing
+after cloning the repository:
+1. run `python convert-latex.py` in the main directory
+    * when prompted for a ".txt filename", enter `example`
+2. run `sent example.txt` in the same directory
+if the sent presentation displays what it says it should display, you should be good.
+
+## how to use
+to use this, first copy the `convert-latex.py` file into the same directory as the txt file you want to read with sent.
+
+now, edit your txt file so that each equation is on its own page (empty line both before and after it). An example follows:
+
+```
+first
+slide
+
+a command to display the latex equation "L=3" follows:
+
+$L=3
+
+final slide
+```
+
+make sure that you format it with  **only one** dollar sign in front. otherwise pdflatex will get have an aneurysm and your computer will explode.
+
+finally, run `python convert-latex.py` and when prompted, enter the name of your txt file (without the .txt). a lot of scary text will pop up (!) but then you will have some nice png files which will be referenced with proper syntax
+
+now you can do `sent (filename).txt` and you will have epic gamer moment
+
+## disclaimer
+i don't know how to code
---
+++ convert-latex.py
@@ -0,0 +1,57 @@
+import os
+# get filename
+filename = input("Please enter the .txt filename: ")
+
+# open file to read it
+file = open(filename + ".txt", "r")
+
+# read file lines into a list
+file_line_list = file.readlines()
+print("This is the file_line_list:")
+print(file_line_list)
+file.close()
+
+# get the lines with the latex demarcator in them (line begins with $)
+latex_lines = []
+for i in file_line_list:
+    if i[0] == '$':
+        latex_lines.append(i)
+
+## skeleton for latex .tex file
+before_eq_string = r"\documentclass[preview]{standalone}\begin{document}$"
+after_eq_string = r"$\end{document}"
+
+# run the lines with the latex demarcator through the latex2png script or something
+for i in range(len(latex_lines)):
+    ## get the names for the pdf and png files (`filename-latex-1`, `filename-latex-2`, etc.)
+    latex_filename = filename + '-latex-' + str(i)
+
+    ## create the .tex file from skeleton and latex_line in question
+    latex_equation = latex_lines[i][1:]
+    tex_file_contents = before_eq_string + latex_equation + after_eq_string
+    tex_file = open(latex_filename + ".tex", "w")
+    tex_file.write(tex_file_contents)
+    tex_file.close()
+
+    ## create the pdf (`pdflatex -jobname='filename to write' file.tex`)
+    # to specify output name: -jobname=STRING flag before the FILE flag at the end
+    os.system('pdflatex ' + latex_filename + '.tex')
+
+    ## create the png from the pdf (`convert -density 3000 file.pdf -quality 90 file.png`)
+    os.system('convert -density 3000 ' + latex_filename + '.pdf -quality 90 ' + latex_filename + '.png')
+
+    ## remove all the useless (.aux, .log, .pdf, .tex) latex files
+    os.system('rm *.log *.aux *.pdf *.tex')
+
+# write a new file with the png references in it and overwrite the original filename
+latex_line_count = 0
+for i in range(len(file_line_list)):
+    if file_line_list[i] != '\n' and file_line_list[i][0] == '$':
+        file_line_list[i] = '@' + filename + '-latex-' + str(latex_line_count) + '.png\n'
+        latex_line_count += 1
+
+file = open(filename + ".txt", "w")
+file.write(''.join(file_line_list))
+print("This is the new file_line_list:")
+print(file_line_list)
+file.close()
